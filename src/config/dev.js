34ed0f0922e558154ae67e82d1b82c99225d97bb
scove93 <seancoverdill@gmail.com>
2019-05-29 19:53:36 -0400

    'Update README'

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,8 +1,9 @@
 export const FirebaseConfig = {
-  apiKey: "AIzaSyAT0cyZbPD0fg6bHzVfHCTu2c4j2wGe7Ys",
-  authDomain: "firedux-todo.firebaseapp.com",
-  databaseURL: "https://firedux-todo.firebaseio.com",
-  projectId: "firedux-todo",
-  storageBucket: "firedux-todo.appspot.com",
-  messagingSenderId: "750399659188"
-}
+  apiKey: "AIzaSyCBSxCg6v7MI_ZfN4XinhiGVQFXrYiSJfk",
+  authDomain: "firedux-todoapp.firebaseapp.com",
+  databaseURL: "https://firedux-todoapp.firebaseio.com",
+  projectId: "firedux-todoapp",
+  storageBucket: "firedux-todoapp.appspot.com",
+  messagingSenderId: "370806937407",
+  appId: "1:370806937407:web:092d2188c360889c"
+};
